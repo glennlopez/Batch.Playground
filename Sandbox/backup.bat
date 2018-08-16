8522c03e04105c0b03e0d0f3d618af5a679ad019
Glenn Lopez <glennlopez@gmail.com>
2018-08-16 14:56:07 -0500

    update

--- Sandbox/backup.bat
+++ Sandbox/backup.bat
@@ -1,12 +1,22 @@
-@echo off
-set target=C:\Backup
-set source=C:\Source
-set backupcmd=xcopy /s /c /d /e /h /i /r /y
+@ECHO off
+SET target=C:\Backup
+SET source=C:\Source
+SET backupcmd=xcopy /s /c /d /e /h /i /r /y /z
+SET archiveDate=%DATE:~10,4%.%DATE:~4,2%.%DATE:~7,2%
+SET archiveName=MasterDB
 
-:: backup changes made
-echo ### Backing up target folder...
-::%backupcmd% "%USERPROFILE%\My Documents" "%target%\My Documents"
+:: Backup routine
+REM Quick Backup (backup new & modified files only) 
+ECHO Copying the following changes to "%target%":
 %backupcmd% "%source%" "%target%"
 
-echo Backup Script Done!
+:: Backup version control
+REM Compresses the up-to-date backup folder (.zip)
+ECHO.
+ECHO Creating archive of new backup (%target%\ARCHIVES\)...
+ECHO Note: If %archiveName%.%archiveDate%.zip already exists, it will be updated.
+powershell.exe -command "& { Compress-Archive -Update -Path %source%\* -CompressionLevel Optimal -DestinationPath %target%\ARCHIVES\%archiveName%.%archiveDate%.zip; }"
+
+ECHO.
+ECHO Backup routine complete!
 @pause
