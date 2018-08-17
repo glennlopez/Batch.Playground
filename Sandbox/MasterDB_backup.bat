548f1470348eb76b2417b750dc17cc3d0796386c
Glenn Lopez <glennlopez@gmail.com>
2018-08-17 09:48:05 -0500

    update

--- Sandbox/MasterDB_backup.bat
+++ Sandbox/MasterDB_backup.bat
@@ -1,23 +1,17 @@
 @ECHO off
+
+:: SET Parameters
 SET source=\\Pct-fs\d\SIMULA~1\MasterDB
 SET target=\\Pct-fs\e\MasterDB_backup
 SET backupcmd=xcopy /s /c /d /e /h /i /r /y /z
-SET archiveDate=%DATE:~7,2%
 SET archiveName=MasterDB
 
-:: Backup routine
-REM Quick Backup (backup new & modified files only) 
+:: Backup & Compression routine
 ECHO Copying the following changes to "%target%":
 %backupcmd% "%source%" "%target%"
-
-:: Backup version control
-REM Compresses the up-to-date backup folder (.zip)
 ECHO.
-ECHO Creating archive of new backup (%target%\ARCHIVES)...
-ECHO Note: If archive (MasterDB.zip) already exists, it will be updated.
+ECHO Archiving (.zip) MasterDB folder to "%target%\ARCHIVES".
 powershell.exe -command "& { Compress-Archive -Update -Path %source%\* -CompressionLevel Optimal -DestinationPath %target%\ARCHIVES\%archiveName%-$(get-date -f yyy-MM-dd).zip; }"
-
-
 ECHO.
 ECHO Backup routine complete!
-@pause
+@PAUSE
