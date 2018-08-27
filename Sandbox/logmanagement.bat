1a98f9b52b61270669cab9b59f72055023a334da
Glenn Lopez <glennlopez@gmail.com>
2018-08-26 20:50:48 -0500

    update

---
+++ Sandbox/logmanagement.bat
@@ -0,0 +1,15 @@
+@ECHO off
+SET appliance=PCT
+
+ECHO Use Log Management
+ECHO [1] Clear %appliance% usage logs.
+ECHO [2] Collect %appliance% usage logs.
+
+REM Clear logs
+ECHO 
+
+REM Collect logs
+ECHO 
+
+ECHO Script end!
+@PAUSE
